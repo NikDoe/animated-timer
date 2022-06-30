568b3d712e75694fda196bff19d40a8327a56ec3
NikDoe <nikd0e@yandex.ru>
2022-06-30 18:25:03 +0300

    binding events in class

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Hello there')
+const durationInput = document.querySelector('#duration');
+const startBtn = document.querySelector('#start');
+const pauseBtn = document.querySelector('#pause');
+
+class Timer {
+	constructor(durationInput, startBtn, pauseBtn) {
+		this.d = durationInput;
+		this.s = startBtn;
+		this.p = pauseBtn;
+
+		this.s.addEventListener('click', this.start);
+	}
+
+	start() {
+		console.log('Hi there');
+	}
+}
+
+const timer = new Timer(durationInput, startBtn, pauseBtn);
