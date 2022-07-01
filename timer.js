dcd1348c1eae3e254ad161e2be11dde1fec21525
NikDoe <nikd0e@yandex.ru>
2022-07-01 16:54:39 +0300

    refactor(timer): extracting timer code

---
+++ timer.js
@@ -0,0 +1,44 @@
+class Timer {
+	constructor(durationInput, startBtn, pauseBtn, callbacks) {
+		this.d = durationInput;
+		this.s = startBtn;
+		this.p = pauseBtn;
+
+		if (callbacks) {
+			this.onStart = callbacks.onStart;
+			this.onTick = callbacks.onTick;
+			this.onComplete = callbacks.onComplete;
+		}
+
+		this.s.addEventListener('click', this.start);
+		this.p.addEventListener('click', this.pause);
+	}
+
+	start = () => {
+		if (this.onStart) this.onStart();
+		this.tick();
+		this.interval = setInterval(this.tick, 1000);
+	};
+
+	pause = () => {
+		clearInterval(this.interval);
+	};
+
+	tick = () => {
+		if (this.timeRemaining <= 0) {
+			this.pause();
+			if (this.onComplete) this.onComplete();
+		} else {
+			this.timeRemaining = this.timeRemaining - 1;
+			if (this.onTick) this.onTick();
+		}
+	};
+
+	get timeRemaining() {
+		return parseFloat(this.d.value);
+	}
+
+	set timeRemaining(time) {
+		this.d.value = time;
+	}
+}
