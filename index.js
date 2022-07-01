582fb182b2de307d61d09b403209db8634282194
NikDoe <nikd0e@yandex.ru>
2022-07-01 18:33:15 +0300

    feature(timer): animate svg circle border w/ tick

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const durationInput = document.querySelector('#duration');
 const startBtn = document.querySelector('#start');
 const pauseBtn = document.querySelector('#pause');
+const circle = document.querySelector('circle');
 
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', perimeter);
+
+let currentOffset = 0;
 const timer = new Timer(durationInput, startBtn, pauseBtn, {
 	onStart() {
 		console.log('Timer started');
 	},
 	onTick() {
-		console.log('Timer is ticked down');
+		circle.setAttribute('stroke-dashoffset', currentOffset);
+		currentOffset = currentOffset - 50;
 	},
 	onComplete() {
 		console.log('Timer is completed');
