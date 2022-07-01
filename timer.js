fb9750783310c71abb8568cd511e328c9a0f2e44
NikDoe <nikd0e@yandex.ru>
2022-07-01 19:01:37 +0300

    fix(timer): fix the offset value

--- timer.js
+++ timer.js
@@ -15,7 +15,7 @@ class Timer {
 	}
 
 	start = () => {
-		if (this.onStart) this.onStart();
+		if (this.onStart) this.onStart(this.timeRemaining);
 		this.tick();
 		this.interval = setInterval(this.tick, 10);
 	};
@@ -30,7 +30,7 @@ class Timer {
 			if (this.onComplete) this.onComplete();
 		} else {
 			this.timeRemaining = this.timeRemaining - 0.01;
-			if (this.onTick) this.onTick();
+			if (this.onTick) this.onTick(this.timeRemaining);
 		}
 	};
 
